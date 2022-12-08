e9052f89f2324d82f291a16a5c5d2c2e5771c182
lny <lny@163.com>
2022-12-08 16:47:10 +0800

    添加eureka模板，并把支付模板和消费者模板注册到eureka

---
+++ lny_springcloud/cloud-eureka-server7001/src/main/java/com/lny/springcloud/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.lny.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //说明这个启动类是eureka服务中心
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
